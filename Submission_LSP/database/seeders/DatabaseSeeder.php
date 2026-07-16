c0996d78108ad9bd70d7a5c5a554c44eea10d081
BONNIEVITTO ALZAIN <bonnievittoalzain_2327250050@mhs.mdp.ac.id>
2026-07-16 09:48:30 +0700

    feat: Update Seeder

--- Submission_LSP/database/seeders/DatabaseSeeder.php
+++ Submission_LSP/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Doctor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -49,6 +50,45 @@ class DatabaseSeeder extends Seeder
             'verified_at' => now(),
         ]);
 
-        $this->call(DoctorSeeder::class);
+        // 1. Dokter Penyakit Dalam
+        $doctor1 = Doctor::create([
+            'name' => 'dr. Andi Pratama, Sp.PD',
+            'specialization' => 'Penyakit Dalam',
+        ]);
+        
+        // Memasukkan jadwal untuk Dokter 1
+        $doctor1->schedules()->createMany([
+            ['day' => 'Senin', 'start_time' => '08:00:00', 'end_time' => '12:00:00']
+        ]);
+
+        // 2. Dokter Gigi
+        $doctor2 = Doctor::create([
+            'name' => 'drg. Siti Nurhaliza',
+            'specialization' => 'Dokter Gigi',
+        ]);
+        
+        $doctor2->schedules()->createMany([
+            ['day' => 'Selasa', 'start_time' => '09:00:00', 'end_time' => '14:00:00']
+        ]);
+
+        // 3. Dokter Umum
+        $doctor3 = Doctor::create([
+            'name' => 'dr. Budi Setiawan',
+            'specialization' => 'Umum',
+        ]);
+        
+        $doctor3->schedules()->createMany([
+            ['day' => 'Selasa', 'start_time' => '13:00:00', 'end_time' => '17:00:00']
+        ]);
+
+        // 4. Dokter Anak
+        $doctor4 = Doctor::create([
+            'name' => 'dr. Maya Anggraini, Sp.A',
+            'specialization' => 'Anak',
+        ]);
+
+        $doctor4->schedules()->createMany([
+            ['day' => 'Selasa', 'start_time' => '13:00:00', 'end_time' => '17:00:00']
+        ]);
     }
 }
